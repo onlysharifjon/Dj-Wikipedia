4dcf1b6592493de816b983349507ae57d1efe592
winchestor2001 <sbexruz2001@gmail.com>
2023-12-07 15:00:45 +0500

    configure setting.py, urls, templates

---
+++ core/urls.py
@@ -0,0 +1,13 @@
+from django.conf import settings
+from django.contrib import admin
+from django.urls import path, include, re_path
+from django.views.static import serve
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    path('accounts/', include('accounts.urls')),
+    path('encyclopedia/', include('encyclopedia.urls')),
+
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+]
